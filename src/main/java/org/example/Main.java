6d04f63461b786e83a44b31865290c3e683c961c
jhs3844 <jhs3844@gmail.com>
2024-08-06 20:20:55 +0900

    코딩테스트

---
+++ src/main/java/org/example/Main.java
@@ -0,0 +1,25 @@
+package org.example;
+
+import java.sql.Array;
+import java.util.HashSet;
+import java.util.Set;
+
+public class Main {
+    public static void main(String[] args) {
+        Set<Integer> result = new HashSet<>();
+        int[] nums = {3,1,2,3};
+        int shot = nums.length / 2;
+//        result.add(nums[0]);
+
+        for (int i=0; i < nums.length ; i++) {
+            result.add(nums[i]);
+        }
+        if (result.size() >= shot) {
+            System.out.println("resulut : " + shot);
+        } else if(result.size() < shot) {
+            System.out.println("resulut : " + result.size());
+        }
+
+
+    }
+}
