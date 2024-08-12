461ba22a0f4ba5f2f5aee9aa4388e7c53716095d
jhs3844 <jhs3844@gmail.com>
2024-08-12 22:22:38 +0900

    코딩테스트 마라톤

--- src/main/java/org/example/hash/marathon_level1.java
+++ src/main/java/org/example/hash/marathon_level1.java
@@ -2,6 +2,35 @@ package org.example.hash;
 import java.util.*;
 public class marathon_level1 {
     public static void main(String[] args) {
+        test3();
+    }
+
+    public static void test1() {
+        String[] participant = {"leo", "kiki", "eden"};
+        String[] completion = {"leo", "kiki"};
+
+        List<String> pt = new ArrayList<>();
+        List<String> cp = new ArrayList<>();
+        for (String name : participant) {
+            pt.add(name);
+        }
+        for (String nm : completion) {
+            cp.add(nm);
+        }
+
+        System.out.println("pt :" + pt);
+        System.out.println("cp :" + cp);
+
+        for (int i=0; i < cp.size(); i++) {
+            if (pt.contains(cp.get(i))) {
+                pt.remove(cp.get(i));
+            }
+        }
+
+        System.out.println(pt.get(0));
+    }
+
+    public static void test2() {
         List<String> participant = new ArrayList<>(List.of("leo", "kiki", "eden"));
         List<String> completion = new ArrayList<>(List.of("eden", "kiki"));
 
@@ -12,6 +41,26 @@ public class marathon_level1 {
             }
         }
 
-        System.out.println("participant : " + participant.getFirst());
+        System.out.println("participant : " + participant.get(0));
     }
+
+    public static void test3() {
+        String[] participant = {"marina", "josipa", "nikola", "vinko", "filipa"};
+        String[] completion = {"josipa", "filipa", "marina", "nikola"};
+
+        Arrays.sort(participant);
+        Arrays.sort(completion);
+
+        System.out.println("sorting participant : " + Arrays.stream(participant).toList());
+        System.out.println("sorting completion : " + Arrays.stream(completion).toList());
+        int i = 0;
+        for (i=0; i<completion.length; i++) {
+
+            if (!participant[i].equals(completion[i])) {
+                System.out.println(participant[i]);
+            }
+        }
+        System.out.println(participant[i]);
+    }
+
 }
